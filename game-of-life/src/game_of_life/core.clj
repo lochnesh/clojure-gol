59b70c07ca7d8829354acf7b0a0f09d09526df70
Skyler Nesheim <skylernesheim@gmail.com>
2012-10-18 23:14:33 -0500

    cell with two neighbors lives

--- game-of-life/src/game_of_life/core.clj
+++ game-of-life/src/game_of_life/core.clj
@@ -1,15 +1,25 @@
 (ns game-of-life.core)
 
+(defn count-neighbors
+	[cell,life-board]
+	(def row (get cell 0))
+	(def col (get cell 1))
+    (+ 
+	  (if (contains? life-board [(- row 1),col]) 1 0)
+	  (if (contains? life-board [(+ row 1),col]) 1 0)))
+
 (defn play-life
 	[life-board]
 	
 	;all cells die if board has two or less live cells
-	(if (<= (count life-board) 2) '())) 
+    (if (<= (count life-board) 2) #{}
+		(set (filter (fn [it] (>= (count-neighbors it life-board) 2)) life-board))))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
   (println args "Hello, World!")
-  (def life-board '())
-  (play-life life-board))
+  (def life-board #{})
+  (play-life life-board)
+  (count-neighbors [2,1] #{[2,1],[1,1]}))
 
--- game-of-life/test/game_of_life/core_test.clj
+++ game-of-life/test/game_of_life/core_test.clj
@@ -7,9 +7,25 @@
     (is (= true true))))
 
 (deftest life-empty
-  (testing "life with empty list"
-    (is (= '() (play-life '())))))
+  (testing "life with empty list return empty list"
+    (is (= #{} (play-life #{})))))
 
 (deftest life-one-cell
-	(testing "life with one cell"
-	(is (= '() (play-life '("1"))))))
+	(testing "life with one cell returns empty list"
+	  (is (= #{} (play-life #{[1,1]})))))
+	
+(deftest life-two-cell
+	(testing "life with two cells returns empty list"
+	  (is (= #{} (play-life #{[1,1],[2,1]})))))
+	
+(deftest two-neighbors-lives
+	(testing "cell with two neighbors lives"
+	  (is (= #{[1,1]} (play-life #{[0,1],[1,1],[2,1]})))))
+	
+(deftest one-neighbor
+	(testing "finds one neighbor"
+		(is (= 1 (count-neighbors [1,1] #{[0,1],[1,1]})))))
+		
+(deftest two-neighbors
+	(testing "finds two neighbors"
+		(is (= 2 (count-neighbors [1,1] #{[0,1],[1,1],[2,1]})))))
