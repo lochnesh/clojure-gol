10db6509bbf2479ab46653f7fa6fbc88b19e9ee4
Skyler Nesheim <skylernesheim@gmail.com>
2012-10-22 23:10:20 -0500

    updated project file

--- game-of-life/project.clj
+++ game-of-life/project.clj
@@ -1,6 +1,6 @@
 (defproject game-of-life "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "a clojure implementation of game of life"
+  :url "http://skylernesheim.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
